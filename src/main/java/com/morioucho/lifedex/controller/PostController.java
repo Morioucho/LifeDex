2c517b4ca5bc0b40cc08e5778fab3ad2dce5fcd3
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-07-30 11:52:17 -0700

    Post Service Rework
    Created seperate files for documentation of the posts system as well as made the posts system work on JSON instead of view models.

--- src/main/java/com/morioucho/lifedex/controller/PostController.java
+++ src/main/java/com/morioucho/lifedex/controller/PostController.java
@@ -2,87 +2,41 @@ package com.morioucho.lifedex.controller;
 
 import com.morioucho.lifedex.model.Post;
 import com.morioucho.lifedex.service.PostService;
+import com.morioucho.lifedex.service.TrieService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.ui.Model;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/posts")
 public class PostController {
     @Autowired
     private PostService postService;
 
-    /**
-     * Retrieves all the posts and returns them in a model.
-     *
-     * @param model the model to add attributes and such to
-     * @return a name to display posts
-     */
+    @Autowired
+    private TrieService trieService;
+
     @GetMapping
-    public String getAllPosts(Model model){
-        List<Post> posts = postService.findAll();
-        model.addAttribute("posts", posts);
-
-        return "posts";
+    public List<Post> getAllPosts(){
+        return postService.findAll();
     }
 
-    /**
-     * Retrieves a post via ID and adds it to the model.
-     *
-     * @param id the ID of the post to retrieve
-     * @param model the model to add attributes and such to
-     * @return the view name for displaying posts
-     */
     @GetMapping("/{id}")
-    public String getPostByID(@PathVariable Long id, Model model){
-        Post post = postService.getPostByID(id);
-        model.addAttribute("post", post);
-
-        return "post";
+    public Post getPostByID(@PathVariable Long id){
+        return postService.getPostByID(id);
     }
 
-    /**
-     * Displays the form for making a new post.
-     *
-     * @param model the model to add attributes to
-     * @return the view name for the create post form
-     */
-    @GetMapping("/new")
-    public String createPostForm(Model model){
-        model.addAttribute("post", new Post());
-
-        return "create_post";
+    @PostMapping("/new")
+    public Post createPost(@RequestBody Post post){
+        return postService.createPost(post);
     }
 
-    /**
-     * Creates a new post.
-     *
-     * @param post the post to create
-     * @return a redirect to the list of posts
-     */
-    @PostMapping
-    public String createPost(@ModelAttribute Post post){
-        postService.createPost(post);
-
-        return "redirect:/posts";
-    }
-
-    /**
-     * Deletes a post using its ID
-     *
-     * @param id the ID of the post to delete.
-     * @return a redirect to the list of posts
-     */
-    @GetMapping("/delete/{id}")
-    public String deletePost(@PathVariable Long id){
+    @DeleteMapping("/delete/{id}")
+    public void deletePost(@PathVariable Long id){
         // TODO: Secure this endpoint so that random posts won't be removed.
-        postService.deletePost(id);
 
-        return "redirect:/posts";
+        postService.deletePost(id);
     }
 }
