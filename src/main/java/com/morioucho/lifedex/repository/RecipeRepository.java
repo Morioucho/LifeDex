55bc2ddb500c997c1fa2e57fabb7c2c5ff770be0
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-08-18 11:11:51 -0700

    Cleaning of Old Code
    Removal of all the old code in order to start from a clean slate.

--- src/main/java/com/morioucho/lifedex/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.morioucho.lifedex.repository;
-
-import com.morioucho.lifedex.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
--- src/main/java/com/morioucho/lifedex/trie/TrieType.java
+++
@@ -1,6 +0,0 @@
-package com.morioucho.lifedex.trie;
-
-public enum TrieType{
-    POST,
-    RECIPE
-}
